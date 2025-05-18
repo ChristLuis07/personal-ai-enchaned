6813029e4a4f4ebc06b85e50c6e29393b19803e6
Christian <christianluisginting@gmail.com>
2025-05-18 14:58:04 +0700

    backend integrated

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,39 +1,46 @@
-"use client"
+'use client';
 
-import { Button } from '@/components/ui/button'
+import { Button } from '@/components/ui/button';
+import { AuthContext } from '@/context/AuthContext';
+import { api } from '@/convex/_generated/api';
 import { GetAuthUserData } from '@/services/GlobalApi';
-import { useGoogleLogin } from '@react-oauth/google'
+import { useGoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
-import Image from 'next/image'
-import React from 'react'
+import { useMutation } from 'convex/react';
+import Image from 'next/image';
+import React, { useContext } from 'react';
 
 function SignIn() {
-    const googleLogin = useGoogleLogin({
-        onSuccess: async (tokenResponse) => {
-            if(typeof window !== 'undefined') {
-                localStorage.setItem('user_token', tokenResponse.access_token);
-            }
-            
-            const user = GetAuthUserData(tokenResponse.access_token);
-            console.log(user);
-        },
-        onError: errorResponse => console.log(errorResponse),
-    });
-  return (
-    <div className='flex items-center justify-center h-screen'>
-    <div className='flex items-center flex-col gap-5 border rounded-2xl p-10 shadow-md'>
-        <Image
-        src={'/logo.svg'}
-        alt='logo'
-        width={50}
-        height={50}
-        />
-        <h2 className='text-2xl'>Sign In To Personal Ai Enchaned</h2>
+  const CreateUser = useMutation(api.users.CreateUser);
+  const { user, setUser } = useContext(AuthContext);
+  const googleLogin = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      if (typeof window !== 'undefined') {
+        localStorage.setItem('user_token', tokenResponse.access_token);
+      }
 
-        <Button onClick={()=>googleLogin()}>Sign in With Gmail</Button>
+      const user = await GetAuthUserData(tokenResponse.access_token);
+      console.log(user);
+      // Simpan informasi user
+      const result = await CreateUser({
+        name: user?.name,
+        email: user?.email,
+        picture: user.picture,
+      });
+      setUser(result);
+    },
+    onError: (errorResponse) => console.log(errorResponse),
+  });
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex items-center flex-col gap-5 border rounded-2xl p-10 shadow-md">
+        <Image src={'/logo.svg'} alt="logo" width={50} height={50} />
+        <h2 className="text-2xl">Sign In To Personal Ai Enchaned</h2>
+
+        <Button onClick={() => googleLogin()}>Sign in With Gmail</Button>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default SignIn
+export default SignIn;
---
+++ convex/users.ts
@@ -0,0 +1,32 @@
+import { v } from 'convex/values';
+import { mutation } from './_generated/server';
+
+export const CreateUser = mutation({
+  args: {
+    name: v.string(),
+    email: v.string(),
+    picture: v.string(),
+  },
+  handler: async (ctx, args) => {
+    // Jika user sudah ada di database
+    const user = await ctx.db
+      .query('users')
+      .filter((q) => q.eq(q.field('email'), args.email))
+      .collect();
+
+    if (user?.length == 0) {
+      // jika belum ada, buat user baru
+      const data = {
+        name: args.name,
+        email: args.email,
+        picture: args.picture,
+        credits: 5000,
+      };
+      const result = await ctx.db.insert('users', data);
+
+      return data;
+    }
+
+    return user[0];
+  },
+});
