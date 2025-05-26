0f25a9e02d51f2f95bc209c1c3017b58074898e9
Christian <christianluisginting@gmail.com>
2025-05-26 13:47:31 +0700

    fixing

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
-      <Button>Halo</Button>
+      <h1 className="text-3xl text-blue-400">Home</h1>
+      
+      <Button>Bikin Kopi</Button>
     </div>
   );
 }
